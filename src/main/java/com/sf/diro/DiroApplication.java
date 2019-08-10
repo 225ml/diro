9a2babd74bc741b733f108c5449428eb721bd36f
shifeng <549629859@qq.com>
2019-08-10 12:31:04 +0800

    Initial commit

---
+++ src/main/java/com/sf/diro/DiroApplication.java
@@ -0,0 +1,13 @@
+package com.sf.diro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DiroApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DiroApplication.class, args);
+    }
+
+}
---
+++ src/main/java/com/sf/diro/controller/TestController.java
@@ -0,0 +1,13 @@
+package com.sf.diro.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Controller
+public class TestController {
+   // @RequestMapping("/")
+   /* public String index(){
+        return "index";
+    }*/
+}
